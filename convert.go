29cce9aaf3678ae28b2485a5a5398acf09ee593f
Galdoba <galdoba@mail.ru>
2019-06-24 14:11:32 +1000

    rename: IntToStr = ItoS

--- convert.go
+++ convert.go
@@ -2,13 +2,13 @@ package convert
 
 import "strconv"
 
-//IntToStr - Int в String
-func IntToStr(i int) string {
+//ItoS - Int в String
+func ItoS(i int) string {
 	return strconv.Itoa(i)
 }
 
-//StrToInt - String в Int
-func StrToInt(s string) int {
+//StoI - String в Int
+func StoI(s string) int {
 	in, err := strconv.Atoi(s)
 	if err != nil {
 		panic(err)
